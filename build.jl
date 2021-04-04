0c72292c68e6e0abfd662ef6f4531ddffc4edb56
David Anthoff <anthoff@berkeley.edu>
2021-04-03 17:57:46 -0700

    Set execute permissions more narrowly

--- build.jl
+++ build.jl
@@ -84,7 +84,7 @@ mktempdir() do temp_path
                     cd(root) do
                         for file in files
                             if endswith(file, ".exe") || endswith(file, ".dll") || endswith(file, ".cmd")
-                                run(`chmod +x $file`)
+                                run(`chmod u+x $file`)
                             end
                         end
                     end
